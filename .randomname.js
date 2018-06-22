6b3a711c17e75ccb2cf56a3dc11fbb88e47ce231
Dean Glueckler <glueckler@gmail.com>
2018-06-21 23:48:28 -0700

    added some flags support and magic128 creaation

--- .randomname.js
+++ .randomname.js
@@ -1,25 +1,144 @@
-var fs = require('fs')
-var path = require('path')
+var fs = require("fs");
+var path = require("path");
 
-console.log(`goooood morning, let's randomize some filenames.. ><>\``)
-var prefix = process.argv[2] || ''
-var chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p']
-var random = () => (chars[Math.trunc(Math.random() * 16)])
+console.log(`goooood morning, let's randomize some filenames.. ><>\``);
 
-fs.readdir('./', (err, files) => {
+// function to generate random string
+var chars = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p"
+];
+var random = () => chars[Math.trunc(Math.random() * 16)];
+
+// first argv input
+var argIndex = 2;
+
+// gather all flags passed ie '--128'
+var flags = [];
+var force = false;
+var make128 = false;
+var getFlags = () => {
+  var argument = process.argv[argIndex];
+  if (argument.startsWith("--")) {
+    flags.push(argument);
+    argIndex++;
+    getFlags();
+  }
+};
+getFlags();
+
+// prefix will be the last argument passed in
+var prefix = process.argv[argIndex] || "";
+
+// start checking flags
+if (flags.includes("--128")) {
+  console.log(`making 128`);
+  make128 = true;
+}
+if (flags.includes("--force")) {
+  console.log(`force rename of all files..`);
+  force = true;
+}
+if (flags.includes("--help")) {
+  console.log(`helpp`);
+  return;
+}
+
+var count = 0;
+var deleted = 0;
+fs.readdir("./", (err, files) => {
+  if (err) throw err;
   for (const file of files) {
     // continue if hidden file
-    if (file[0] === '.') continue
-    
+    if (file[0] === ".") continue;
+
+    //continue if dir
+    if (fs.lstatSync(file).isDirectory()) continue;
+
     // delete  if ableton file
-    var extension = path.extname(file)
-    if (extension === '.asd') {
-      fs.unlinkSync(file)
-      console.log(`ableton file: ${file} was deleted..`)
-      continue
+    var extension = path.extname(file);
+    if (extension === ".asd") {
+      fs.unlinkSync(file);
+      console.log(`ableton file: ${file} was deleted..`);
+      deleted++;
+      continue;
     }
+
+    // skip files with .lock (ie file already processed before)
+    if (file.includes(".lock") && !force) continue;
+
     // generate random string
-    var rdmStr = ['r','a','n','d','o','m'].map(() => (random())).join('')
-    fs.renameSync(file, `${prefix}${rdmStr}${extension}`)
+    var rdmStr = ["r", "a", "n", "d", "o", "m"].map(() => random()).join("");
+    fs.renameSync(file, `${prefix}${rdmStr}.lock${extension}`);
+    count++;
   }
-})
+  console.log(`${count} files processed and renamed`);
+  console.log(`${deleted} files deleted`);
+
+  // now make 128 dir if necessary..
+  if (make128) {
+    // ---------------
+    // function to rm -rf
+    var deleteDir = function(path) {
+      if (path === "/") {
+        console.log("lets not delete our computer..");
+        return;
+      }
+      if (fs.existsSync(path)) {
+        fs.readdirSync(path).forEach(function(file, index) {
+          var curPath = path + "/" + file;
+          if (fs.lstatSync(curPath).isDirectory()) {
+            // recurse
+            deleteFolderRecursive(curPath);
+          } else {
+            // delete file
+            fs.unlinkSync(curPath);
+          }
+        });
+        fs.rmdirSync(path);
+      }
+    };
+
+    // -------------
+
+    console.log(`making random 128 `);
+    deleteDir("./magic128");
+    fs.mkdirSync("./magic128");
+
+    var filesInDir = fs.readdirSync("./");
+    var dir128 = [];
+    const findFileNotDir = () => {
+      var randomIndex = Math.trunc(Math.random() * filesInDir.length);
+      var file = filesInDir[randomIndex];
+      filesInDir.splice(randomIndex, 1);
+      if (fs.lstatSync(file).isDirectory()) return findFileNotDir();
+      else return file;
+    };
+    Array(128)
+      .join(" ")
+      .split(" ")
+      .map(() => {
+        return findFileNotDir();
+      })
+      .forEach(file => {
+        fs.copyFile(file, `./magic128/${file}`, err => {
+          if (err) throw err;
+          console.log(`${file} was coppied to ./magic128/${file}`);
+        });
+      });
+  }
+});
